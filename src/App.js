f42ad91ffa871e9a868382e9ac159bf2c87c2426
Priyadharshini6190 <ppriya6190@gmail.com>
2024-10-22 02:07:14 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>PRIYADHARSHINI S</h1>
+      <h2>Contact information</h2>
+      <p>Address:Sirkali,Mayiladuthurai-609 115
+        <br />
+        Email id:ppriya6190@gmail.com
+        <br />
+        Mobile no:8637430227
+      </p>
+      <h2>Summary</h2>
+      <p>Highly motivated and detailed oriented computer science engineer with a strong academic
+        record.Skilled in web development and operating system.proeficient in programming languages
+        such as HTML/CSS,C programing.Looking to join a dynamic team and grow as a softwore
+        professional.
+      </p>
+
+      <h2>Education</h2>
+      <p>Bachelor of Engineering in Computer science and Engineering
+        <br />
+        Vetri Vinayaga college of Engineering and Technology
+        <br />
+        <h2>Skills</h2>
+        <p><h3>Technical skills </h3>
+          -Web development<br />
+          -Data structure and algorithm
+          <br />
+          -Git
+          <br /></p>
+        <h3>soft skills</h3>
+        -Communication<br />
+        -Team work
+        <br />
+      </p>
+      <h2>Certification</h2>
+      <p>-Full stack development<br />
+        -Artificial Intelligence
+      </p>
+    </div>
+  );
+}
+
+export default App;
